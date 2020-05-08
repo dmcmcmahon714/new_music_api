8313018045f39b4b9960b866a146d2c6c9820aba
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-05-08 11:45:17 -0500

    seeded database with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do 
+	Entry.create(
+		artist: Faker::Music.band,
+		title: Faker::Music.album,
+		date: Faker::Business.credit_card_expiry_date
+		)
+end
+
+puts "Seeded database"
